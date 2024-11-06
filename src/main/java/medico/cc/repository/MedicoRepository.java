ab0386c1975f338769d891b1c962cb8ccb4bcb96
Albertolj <priegoalberto@gmail.com>
2024-11-06 20:23:24 +0100

    Revert "Se han creado los archivos necesarios para crear los endpoints de medico y paciente."
    This reverts commit 4335f1f9a3ae4e3850e6b93efc89bfc9b8a2a684.

--- src/main/java/medico/cc/repository/MedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package medico.cc.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import medico.cc.entity.Medico;
-
-public interface MedicoRepository extends CrudRepository<Medico, String>{
-
-}
--- src/main/java/medico/cc/repository/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package medico.cc.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import medico.cc.entity.Paciente;
-
-public interface PacienteRepository extends CrudRepository<Paciente, String>{
-
-}
